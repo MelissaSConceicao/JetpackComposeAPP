57e6bba3fc3769f07bf1832caa831e73911298f0
Aluno <melissa.conceicao01@etec.sp.gov.br>
2022-11-07 10:33:20 -0300

    JetpackCompose 2

--- app/src/main/java/com/example/aulateste/MainActivity.kt
+++ app/src/main/java/com/example/aulateste/MainActivity.kt
@@ -3,42 +3,60 @@ package com.example.aulateste
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.aulateste.ui.theme.AulaTesteTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContent: bloco que define o layout do app a partir da funções
         setContent {
-            AulaTesteTheme {
-                //chamada da função "Greeting", com o parâmetro "name" e valor "Android"
-                Greeting("Android")
-            }
-
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
+
     }
 }
+data class Message(val author: String, val body: String)
+
 //notação para tornar uma função composta(uma função chama a outra)
 @Composable
-//função Greeting com o parâmetro "name" tipo String
-fun Greeting(name: String) {
+fun MessageCard(msg: Message) {
     //função Text
-    Text(text = "Hello $name!")
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 //Notação que permite visualizar o resultado sem precisar emular o app (só pode ser usada com funções sem parâmetro)
 @Preview
 @Composable
-//essa função permite visualizar o resultado do mesmo código feito acima
-fun DefaultPreview() {
-    AulaTesteTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+    )
+
 }
