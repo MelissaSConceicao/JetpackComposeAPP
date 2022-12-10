3631631f47b9a35fd164447c6bf8950306c6c697
Robson Conceicâo <melissa.conceicao01@etec.sp.gov.br>
2022-12-10 15:42:40 -0300

    JetpackCompose v4

--- app/src/main/java/com/example/aulateste/MainActivity.kt
+++ app/src/main/java/com/example/aulateste/MainActivity.kt
@@ -4,33 +4,34 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.aulateste.ui.theme.AulaTesteTheme
-//Agora vamos usar o Material Desing no projeto, um conjunto de elementos de iu (interface do usuário) prontos para uso
-//O Material Desing foi criado com base em cor, tipografia e forma
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContent: bloco que define o layout do app a partir da funções
         setContent {
-            //tanto no setContent quanto no @Preview, as funções AulaTesteThem e Surface estão ligadas/herdeiras do Material Design
+            //AulaTesteTheme ligado ao Material Desing
             AulaTesteTheme() {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    //Aqui estamos chamando a composição MessageCard que está chamando o objeto Message e dando o valor das propriedades author e body
-                    MessageCard(Message("Android", "Jetpack Compose"))
-                }
+                //Função para gerar várias mensagens a partir do arquivo SampleData e a variável conversationSample
+                Conversation(SampleData.conversationSample)
             }
         }
 
@@ -38,6 +39,14 @@ class MainActivity : ComponentActivity() {
 }
 //aqui estamos declarando as variáveis que serão as propriedades de Message, author e body, ambas do tipo String
 data class Message(val author: String, val body: String)
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    //LazyColumn/LazyRow - processam apensa elementos que aparecem na tela
+    LazyColumn {
+        messages.map { item { MessageCard(it) } }
+    }
+}
 
 //notação para tornar uma função composta(uma função chama a outra)
 @Composable
@@ -57,14 +66,27 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                    //Na modificação dessa borda, temos um uso do Material Desing, em forma da configuração de uma cor
+                //Na modificação dessa borda, temos um uso do Material Desing, em forma da configuração de uma cor
                 .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
         )
         //um espaço entre a imagem e os textos, de largura 8 dp
         Spacer(modifier = Modifier.width(8.dp))
 
-        //O próximo elemento é uma coluna com dois textos: o nome do autor da mensagem e a mensagem
-        Column {
+        /*
+        para fazermos a animação de abrir o restante do conteúdo da mensagem, precisamos armazenar o estado da mensagem
+        (se a mensagem está aberta ou não) na memória usando o remember, que pega o valor do estado através do mutableStateOf
+        */
+        var isExpanded by remember { mutableStateOf(false) }
+
+
+        /*Aqui é uma coisa semelhante com o estado da mensagem. Mas agora, se uma mensagem foi aberta, a cor da mensagem
+        vai mudar gradualmente da surface(branco) até a da primária(roxo), e ao ser fechada acontecerá o contrário*/
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        //A mensagem é clicável, e quando foi clicada, o conteúdo vai aparecer
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 //Nome do autor da mensagem
                 text = msg.author,
@@ -76,39 +98,37 @@ fun MessageCard(msg: Message) {
             //Espaço vertical de 4 dp entre os dois textos
             Spacer(modifier = Modifier.height(4.dp))
 
-            //Aqui temos uma configuração de forma com o Material Desing e a elevação (sombra) do elemento
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                //A cor da surface cor vai se tornar gradualmente a cor primária citada acima
+                color = surfaceColor,
+                //animateContentSize vai mudar o tamanho da surface gradualmente, conforme as mensagens são abertas
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
-                    //Mensagem
                     text = msg.body,
-                    //Espaço em volta do conteúdo do elemento de 4 dp
                     modifier = Modifier.padding(all = 4.dp),
-                    //Outra seleção de tipografia com Material Desing
-                    style = MaterialTheme.typography.subtitle1
+                    /*se a mensagem não estiver aberta, só será mostrado 1 linha de conteúdo, caso contrário,
+                    o número máximo de linhas serão todas as linhas da mensagem*/
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
                 )
             }
         }
     }
 }
 
-//Notação que permite visualizar o resultado sem precisar emular o app (só pode ser usada com funções sem parâmetro)
-@Preview(name = "Light Mode")
-@Preview(
-    //Criação de um Moddo Escuro usando Material Desing, os textos e os planos de fundo são adaptados automaticamente
-    //Essas opções de cores para temas claros ou escuros estão no arquivo theme.kt no diretório ui.theme, juntamente com as cores, formas e tipografia
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
+//A partir daqui, temos só as pré-visualizações
 
+//Notação que permite visualizar o resultado sem precisar emular o app (só pode ser usada com funções sem parâmetro)
+@Preview
 @Composable
-fun PreviewMessageCard() {
-    //Aqui na pré-visualização, o esperado é aparecer um card de mensagem com a imagem do perfil, o autor de nome "Lexi" e a sua mensagem
+fun PreviewConversation() {
     AulaTesteTheme() {
-        Surface{
-            MessageCard(
-                msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
-            )
-        }
+        /*Nessa pré-visualização, o esperado é que apareça uma lista de mensagens vinda do
+        SampleData e que, usando a interatividade, essas mensagens mostrem as animações*/
+        Conversation(SampleData.conversationSample)
     }
 }
